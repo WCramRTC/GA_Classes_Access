c4f6eae67562f0186f597977efc187614db0b928
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-10 00:35:26 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace GA_Classes_Access
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        } // end MainPage()
+
+ 
+    } // end class
+
+
+} // end namespace
